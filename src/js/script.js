e64cd5fd3455aa0ad49dff79367105e0c93d8508
kalidarkhan <kalidarkhan@gmail.com>
2021-06-25 17:50:32 +0600

    250621021-1750

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const   hamburger = document.querySelector('.hamburger'),
         menu = document.querySelector('.menu'),
         close = document.querySelector('.menu__close');
+        // block = document.querySelector('.menu__block');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -9,6 +10,17 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+// function onClickClose(block) { // вызвать в момент показа окна, где elem - окно
+//     function outsideClickListener(event) {
+//         if (!elem.contains(event.target) && isVisible(block)) {  // проверяем, что клик не по элементу и элемент виден
+//             //  elem.style.display = 'none'; //скрыть
+//              menu.classList.remove('active');
+//              document.removeEventListener('click', outsideClickListener);
+//         }
+//     }
+//     document.addEventListener('click', outsideClickListener)
+// }
 
 const   persents = document.querySelectorAll('.skills__percent-percents'),
         indicators = document.querySelectorAll('.skills__percent-indicator span');
